019b0dc816f2f64fa92681256912a86867122995
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2024-01-17 19:49:48 -0500

    feat: add expo dev build notice

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import { NativeModules, Platform } from 'react-native';
 import type { Payload } from './types';
 
 const LINKING_ERROR =
-  `The package 'react-native-wear' doesn't seem to be linked. Make sure: \n\n` +
+  "The package 'react-native-wear' doesn't seem to be linked. Make sure: \n\n" +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+  '- You are not using Expo Go (see https://docs.expo.dev/develop/development-builds/create-a-build/)\n';
 
-const PLATFORM_NOT_SUPPORTED_ERROR = `Platform not supported. This package only supports Android.`;
+const PLATFORM_NOT_SUPPORTED_ERROR =
+  'Platform not supported. This package only supports Android.';
 
 // @ts-expect-error
 const isTurboModuleEnabled = global.__turboModuleProxy != null;
@@ -30,12 +31,8 @@ const WearModule = Platform.select({
   default: moduleProxy(PLATFORM_NOT_SUPPORTED_ERROR),
 });
 
-const RNWear = WearModule
-  ? WearModule
-  : moduleProxy(LINKING_ERROR);
+const RNWear = WearModule ? WearModule : moduleProxy(LINKING_ERROR);
 
-export function send<T extends string>(
-  payload: Payload<T>
-): Promise<any> {
+export function send<T extends string>(payload: Payload<T>): Promise<any> {
   return RNWear.send(payload);
 }
