5b77bf3ef0665e8990066b7336d215e37cba0e48
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2024-01-17 19:50:57 -0500

    feat: update library

--- android/src/main/java/dev/turtlepaw/rnwear/ConnectivityManager.kt
+++ android/src/main/java/dev/turtlepaw/rnwear/ConnectivityManager.kt
@@ -1,29 +1,47 @@
 package dev.turtlepaw.rnwear
 
-import android.app.Activity
-import android.content.Intent
-import android.os.Bundle
 import android.util.Log
-import com.facebook.react.bridge.ActivityEventListener
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.ReadableType
 import com.google.android.gms.wearable.PutDataMapRequest
-import java.time.Instant
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.wearable.Wearable
 import kotlin.coroutines.cancellation.CancellationException
 
-class ConnectivityManager(private val applicationContext: ReactApplicationContext) :
-ActivityEventListener {
+
+class ConnectivityManager(private val applicationContext: ReactApplicationContext) {
   private val dataClient by lazy { Wearable.getDataClient(applicationContext) }
 
   fun sendData(payload: ReadableMap, promise: Promise) {
         try {
           val request = PutDataMapRequest.create(SEND_PATH).apply {
-            dataMap // place payload into data map
+            val iterator = payload.keySetIterator()
+            while(iterator.hasNextKey()){
+              val key = iterator.nextKey()
+              val readableType: ReadableType = payload.getType(key)
+              when (readableType) {
+                ReadableType.String -> {
+                  val stringValue = payload.getString(key)
+                  dataMap.putString(key, stringValue)
+                }
+
+                ReadableType.Number -> {
+                  val numberValue = payload.getDouble(key)
+                  dataMap.putDouble(key, numberValue)
+                }
+
+                ReadableType.Boolean -> {
+                  val booleanValue = payload.getBoolean(key)
+                  dataMap.putBoolean(key, booleanValue)
+                }
+
+                else -> {
+                  // @TODO: Handle other data values
+                }
+              }
+            }
+            dataMap // @TODO: place payload into data map
           }
             .asPutDataRequest()
             .setUrgent()
@@ -43,10 +61,6 @@ ActivityEventListener {
   companion object {
     const val TAG = "RNWear"
     const val SEND_PATH = "/rnwear-send"
-  }
-
-  init {
-    applicationContext.addActivityEventListener(this)
   }
 }
 
